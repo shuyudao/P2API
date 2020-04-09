1fb4c516d0f3d46937c70a76168808888a653bd1
websoloho <2423458891@qq.com>
2020-04-10 04:54:11 +0800

    init

--- config.php
+++ config.php
@@ -4,13 +4,11 @@
  * @email: shuyudao@gmail.com
  * @Date: 2020-04-10 03:29:31
  * @LastEditors: 术与道
- * @LastEditTime: 2020-04-10 04:19:46
+ * @LastEditTime: 2020-04-10 04:36:06
  */
 $config = array(
-    "publicPath"=>"C:\Users\sad\Desktop\\test\\tets\app\controller\\", //公共目录，绝对路径，是下面class所有文件公同的父目录
+    "publicPath"=>"", //公共目录，绝对路径，是下面class所有文件公同的父目录
     "class"=>[
-        "StudentCtrl.php",
-        "IndexCtrl.php"
     ],//含有API接口注释的类文件
     "preface"=>["API接口项目名","项目描述，支持HTML"], //文档首页
     "outpath"=>"C:\Users\sad\Downloads\mydoc" //生成文档的保存位置，必须为目录，绝对路径
